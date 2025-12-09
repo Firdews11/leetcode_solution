f94599002c038c784373d4891e70c640905a3030
Firdews11 <firdewshailu@gmail.com>
2025-12-09 22:48:46 +0300

    Add solution for Find the Pivot Integer

--- LeetCode/Find-the-Pivot-Integer/Solution.py
+++ LeetCode/Find-the-Pivot-Integer/Solution.py
@@ -1,10 +1,8 @@
 1class Solution:
 2    def pivotInteger(self, n: int) -> int:
-3        total = (n*(n +1)) //2
-4        for i in range(n+1):
-5            center = i * (i+1) //2
-6            if center == (total - center +i):
+3        for i in range(n,-1,-1):
+4            a=((n*(n+1)/2)-(i*(i-1)/2))
+5            b=((i*(i+1)/2))
+6            if a==b:
 7                return i
-8
-9        return -1
-10
+8        return -1
