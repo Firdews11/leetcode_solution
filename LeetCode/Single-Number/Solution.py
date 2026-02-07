d82d8354f767bef3905c5fc168efad7aab17d14b
Firdews11 <firdewshailu@gmail.com>
2026-02-07 06:30:06 +0300

    Add solution for Single Number

---
+++ LeetCode/Single-Number/Solution.py
@@ -0,0 +1,11 @@
+1class Solution(object):
+2    def singleNumber(self, nums):
+3        """
+4        :type nums: List[int]
+5        :rtype: int
+6        """
+7        n=0
+8        for i in nums:
+9            n= n^i
+10        return n
+11        
