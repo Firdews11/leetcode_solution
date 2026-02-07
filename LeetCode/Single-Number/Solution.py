6b0c67efc8d3a2f7fc4555ec193348d4710bb5f4
Firdews11 <firdewshailu@gmail.com>
2026-02-07 06:30:09 +0300

    Add solution for Single Number

--- LeetCode/Single-Number/Solution.py
+++ LeetCode/Single-Number/Solution.py
@@ -4,8 +4,11 @@
 4        :type nums: List[int]
 5        :rtype: int
 6        """
-7        n=0
-8        for i in nums:
-9            n= n^i
-10        return n
-11        
+7        # n=0
+8        # for i in nums:
+9        #     n= n^i
+10        # return n
+11        for i in nums:
+12            if nums.count(i)==1:
+13                return i
+14        
