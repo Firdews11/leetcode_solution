3c500bf2a0bca42887da9734e1d4c25441551e3d
Firdews11 <firdewshailu@gmail.com>
2026-02-07 06:40:37 +0300

    Add solution for Group Anagrams

--- LeetCode/Group-Anagrams/Solution.py
+++ LeetCode/Group-Anagrams/Solution.py
@@ -2,13 +2,13 @@
 2class Solution:
 3    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 4        anagram_dic = defaultdict(list)
-5        result = []
-6
-7        for key in strs:
-8            sorted_key = tuple(sorted(key))
-9            anagram_dic[sorted_key].append(key)
-10        """ our dictionary {('a', 'e', 't'): ['eat', 'tea', 'ate'], ('a', 'n', 't'):   ['tan', 'nat'], ('a', 'b', 't'): ['bat']} and we used tuple b/c keys can not be lists """
-11        for key in anagram_dic.values():
-12            result.append(key)
-13
-14        return result
+5
+6        for word in strs:
+7            sorted_word = ''.join(sorted(word))
+8            if sorted_word in anagram_dic:
+9                anagram_dic[sorted_word].append(word)
+10            else:
+11                anagram_dic[sorted_word] = [word]
+12        """ our dictionary {('a', 'e', 't'): ['eat', 'tea', 'ate'], ('a', 'n', 't'):   ['tan', 'nat'], ('a', 'b', 't'): ['bat']} and we used tuple b/c keys can not be lists """
+13        
+14        return list(anagram_dic.values())
