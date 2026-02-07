9198f60dce75e4c935377f3a9ccf31a88532f93f
Firdews11 <firdewshailu@gmail.com>
2026-02-07 06:36:58 +0300

    Add solution for Single Number

--- LeetCode/Single-Number/Solution.py
+++ LeetCode/Single-Number/Solution.py
@@ -1,14 +1,8 @@
-1class Solution(object):
-2    def singleNumber(self, nums):
-3        """
-4        :type nums: List[int]
-5        :rtype: int
-6        """
-7        # n=0
-8        # for i in nums:
-9        #     n= n^i
-10        # return n
-11        for i in nums:
-12            if nums.count(i)==1:
-13                return i
-14        
+1class Solution:
+2    def singleNumber(self, nums: List[int]) -> int:
+3        n = 0 
+4        for i in nums:
+5            n^=i 
+6        return n
+7        # the sign ^ is bitt=wise XOR perform bits of 2 integer
+8
